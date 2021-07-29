590f9595f2e4c88cd54c692924d00a6622f9d501
aisudev <tk.thanatorn@gmail.com>
2021-07-29 21:26:57 +0700

    T_T

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -28,7 +28,8 @@
 // @ is an alias to /src
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
-import { watchEffect } from '@vue/runtime-core'
+// import { watchEffect } from '@vue/runtime-core'
+// import NProgress from 'nprogress'
 // import axios from 'axios'
 export default {
   name: 'EventList',
@@ -48,16 +49,16 @@ export default {
     }
   },
   created() {
-    watchEffect(() => {
-      EventService.getEvents(2, this.page)
-        .then((response) => {
-          this.events = response.data
-          this.totalEvents = response.headers['x-total-count'] // <--- Store it
-        })
-        .catch((error) => {
-          console.log(error)
-        })
-    })
+    // watchEffect(() => {
+    //   EventService.getEvents(2, this.page)
+    //     .then((response) => {
+    //       this.events = response.data
+    //       this.totalEvents = response.headers['x-total-count'] // <--- Store it
+    //     })
+    //     .catch((error) => {
+    //       console.log(error)
+    //     })
+    // })
   },
   computed: {
     hasNextPage() {
@@ -67,6 +68,31 @@ export default {
       // Then check to see if the current page is less than the total pages.
       return this.page < totalPages
     }
+  },
+  // eslint-disable-next-line no-unused-vars
+  beforeRouteEnter(routeTo, routeFrom, next) {
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        next((comp) => {
+          comp.events = response.data
+          comp.totalEvents = response.headers['x-total-count']
+        })
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
+  },
+  // eslint-disable-next-line no-unused-vars
+  beforeRouteUpdate(routeTo, routeFrom, next) {
+    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+      .then((response) => {
+        this.events = response.data
+        this.totalEvents = response.headers['x-total-count']
+        next()
+      })
+      .catch(() => {
+        next({ name: 'NetworkError' })
+      })
   }
 }
 </script>
