227517d813293e50656e4d5312e24022729edcfa
aisudev <tk.thanatorn@gmail.com>
2021-07-29 20:30:47 +0700

    done

--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,23 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.GStore.flashMessage = 'You are succesfully edit ' + this.event.title
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+    }
+  }
 }
 </script>
--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,24 @@
 <template>
   <p>Regstration form here</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    register() {
+      this.GStore.flashMessage =
+        'You are succesfully register ' + this.event.title
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+    }
+  }
 }
 </script>
